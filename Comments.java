0492d727134bd9f68bb430977339d93d8b272a24
Akhila-j22 <juturuakhilareddy@gmail.com>
2025-06-11 07:48:30 +0530

    Add files via upload

---
+++ Comments.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Comments {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        // Prints a
+        System.out.println(a);
+        // Prints c
+        System.out.println(c);
+        //prints b
+        System.out.println(b);
+    }
+}
