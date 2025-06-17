6e5eef81637bda082df3500ff47857dd5c0b3a8b
Akhila-j22 <juturuakhilareddy@gmail.com>
2025-06-17 17:43:47 +0530

    Add files via upload

---
+++ Variables and Data Types/ConversionDouble.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class ConversionDouble{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String num = sc.nextLine();
+        int val = Integer.parseInt(num);
+        System.out.println(val*2);
+    }
+}//
