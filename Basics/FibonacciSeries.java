267eb66b30e1a905810b27ac661cbc8427121902
Akhila-j22 <juturuakhilareddy@gmail.com>
2025-06-17 21:34:52 +0530

    Update FibonacciSeries.java

--- Basics/FibonacciSeries.java
+++ Basics/FibonacciSeries.java
@@ -8,8 +8,6 @@ public class FibonacciSeries {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        // Fibonacci శ్రేణి n వరకు ముద్రించు
         for (int i = 0; i < n; i++) {
             System.out.print(fibonacci(i) + " ");
         }
