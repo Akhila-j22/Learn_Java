c3d3759c90d4c899e69aa8710f317aa82e5b069c
Akhila-j22 <juturuakhilareddy@gmail.com>
2025-06-17 19:37:35 +0530

    Add files via upload

---
+++ Basics/SumOfDigitsofNumber.java
@@ -0,0 +1,10 @@
+public class SumOfDigitsofNumber{
+    public static void main(String[]args){
+        int num = 12345;
+        int sum =0;
+        while(num!=0){
+            sum+=num%10;
+            num=num/10;
+        }System.out.println(sum);
+    }
+}
