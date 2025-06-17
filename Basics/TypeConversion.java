73b47f03cebd19fadf10d8785505d0502963516a
Akhila-j22 <juturuakhilareddy@gmail.com>
2025-06-17 17:49:46 +0530

    Add files via upload

---
+++ Basics/TypeConversion.java
@@ -0,0 +1,29 @@
+public class TypeConversion {
+
+	// Main driver method
+	public static void main(String args[]) {
+		// Declaring byte variable
+		byte b;
+
+		// Declaring and initializing integer and double
+		int i = 257;
+		double d = 323.142;
+
+		// Display message
+		System.out.println("Conversion of int to byte.");
+
+		// i % 256
+		b = (byte)i;
+
+		// Print commands
+		System.out.println("i = " + i + ", b = " + b);
+		System.out.println(
+			"\nConversion of double to byte.");
+
+		// d % 256
+		b = (byte)d;
+
+		// Print commands
+		System.out.println("d = " + d + ", b= " + b);
+	}
+}
