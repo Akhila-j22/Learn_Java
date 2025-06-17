6fd25649fc95c0b8c74efd751bfb011dab879d2b
Akhila-j22 <juturuakhilareddy@gmail.com>
2025-06-17 17:44:36 +0530

    Delete Variables and Data Types directory

--- Variables and Data Types/ConversionDouble.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class ConversionDouble{
-    public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        String num = sc.nextLine();
-        int val = Integer.parseInt(num);
-        System.out.println(val*2);
-    }
-}//
