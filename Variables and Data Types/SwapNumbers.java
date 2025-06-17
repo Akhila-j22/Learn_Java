6044ee510cd95f3f289021ae289b6488be8263a1
Akhila-j22 <juturuakhilareddy@gmail.com>
2025-06-17 17:42:21 +0530

    SwapNumbers.java

---
+++ Variables and Data Types/SwapNumbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class SwapNumbers{
+    public static void main(String[]args){
+        int m = 5;
+        int n=6;
+        int temp=m;//swapping the values
+        m=n;
+        n=temp;
+        System.out.println("Value of m is " + m
+                           + " and Value of n is " + n);
+    }
+}
