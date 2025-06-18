9391d2cb03389299c189f23b0c28f962d97ddc5b
Akhila-j22 <juturuakhilareddy@gmail.com>
2025-06-18 07:11:14 +0530

    Update Array.java

--- Arrays/Array.java
+++ Arrays/Array.java
@@ -1,1 +1,13 @@
-arraysArr
+import java.util.*;
+public class LargestNumber{
+  public static void main(String[]args){
+    int arr[] = {12,13,14,10,15};
+    int max = arr[0];
+    for(int i = 0;i<arr.length;i++){
+      if(max<arr[i]){
+        max = arr[i];
+      }
+    }System.out.println(max);
+  }
+}
+    
